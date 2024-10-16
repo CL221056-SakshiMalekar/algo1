08cc096123363ab1498d9d7d8638837920a15667
CL221056-SakshiMalekar <145000349+CL221056-SakshiMalekar@users.noreply.github.com>
2024-10-16 10:43:40 +0530

    Update p7.c

--- p7.c
+++ p7.c
@@ -3,17 +3,17 @@
 int main() {
     int arr[100], n, unique[100], uniqueCount = 0;
 
-    // Input number of elements
+   
     printf("Enter number of elements in the array: ");
     scanf("%d", &n);
 
-    // Input array elements
+   
     printf("Enter %d integers:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Finding unique numbers
+   
     for (int i = 0; i < n; i++) {
         int isUnique = 1;
         for (int j = 0; j < uniqueCount; j++) {
@@ -27,7 +27,7 @@ int main() {
         }
     }
 
-    // Printing unique numbers
+   
     printf("Unique numbers in the array: ");
     for (int i = 0; i < uniqueCount; i++) {
         printf("%d ", unique[i]);
