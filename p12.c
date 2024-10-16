f8d13fc77cb4dbc12a8f5d005f88f455a17aa217
CL221056-SakshiMalekar <145000349+CL221056-SakshiMalekar@users.noreply.github.com>
2024-10-16 10:45:22 +0530

    Update p12.c

--- p12.c
+++ p12.c
@@ -7,9 +7,8 @@ bool printDiamond(int rows) {
         return false;
     }
 
-    int n = rows / 2; // Half the number of rows (integer division)
-
-    // Upper part of the diamond
+    int n = rows / 2; 
+    
     for (int i = 0; i <= n; i++) {
         for (int j = 0; j < n - i; j++) {
             printf(" ");
@@ -20,7 +19,7 @@ bool printDiamond(int rows) {
         printf("\n");
     }
 
-    // Lower part of the diamond
+    
     for (int i = n - 1; i >= 0; i--) {
         for (int j = 0; j < n - i; j++) {
             printf(" ");
