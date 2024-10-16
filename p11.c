276a381c01afbab2c4fe6d1b2c559cab49adbfb5
CL221056-SakshiMalekar <145000349+CL221056-SakshiMalekar@users.noreply.github.com>
2024-10-16 10:45:40 +0530

    Update p11.c

--- p11.c
+++ p11.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 void printHalfDiamond(int columns) {
-    // Upper part of the half diamond
+   
     for (int i = 1; i <= columns; i++) {
         for (int j = 1; j <= i; j++) {
             printf("*");
         }
         printf("\n");
     }
-    // Lower part of the half diamond
+    
     for (int i = columns - 1; i >= 1; i--) {
         for (int j = 1; j <= i; j++) {
             printf("*");
