bc4345d2731498f145b58ac1f9dedabc63c0e5dd
CL221056-SakshiMalekar <145000349+CL221056-SakshiMalekar@users.noreply.github.com>
2024-10-16 10:44:50 +0530

    Update p4.c

--- p4.c
+++ p4.c
@@ -3,14 +3,14 @@
 int main() {
     int numbers[7];
 
-    // Taking input from the user
+    
     printf("Enter 7 integers:\n");
     for (int i = 0; i < 7; i++) {
         printf("Integer %d: ", i + 1);
         scanf("%d", &numbers[i]);
     }
 
-    // Reversing the order of numbers
+    
     printf("Reversed order of numbers: ");
     for (int i = 6; i >= 0; i--) {
         printf("%d ", numbers[i]);
